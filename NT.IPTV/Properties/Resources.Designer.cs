d2243a8c245f54aabeb10b8432589d41afb6be91
Hamdy Ghanem <hamdy.ghanem@hotmail.com>
2024-04-13 14:23:06 -0300

    implemnt vlc and remove media player

--- NT.IPTV/Properties/Resources.Designer.cs
+++ NT.IPTV/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace NT.IPTV.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap small_logo {
+            get {
+                object obj = ResourceManager.GetObject("small logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
